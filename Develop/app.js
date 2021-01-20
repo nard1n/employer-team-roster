abe1a3510caa521f310f911f1971b2bf87b649d2
Nardin Lachowski <nardin@live.com>
2021-01-19 22:31:10 -0500

    added the updated files and screenshot of the app

--- Develop/app.js
+++ Develop/app.js
@@ -10,26 +10,86 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//declaring empty array for employees
+let employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//list of prompts for gathering employee's information
+const prompts = [
+    {
+        type: 'list',
+        name: 'title',
+        message: "Which of the following describes this employee's job title?",
+        choices: ['Manager','Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is this employee's name?"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is this employee's ID number?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is this employee's email address?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is this Engineer's Github username?",
+        when: (answers) => answers.title === 'Engineer'
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Which school does this intern attend?",
+        when: (answers) => answers.title === 'Intern'
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is this manager's office phone number?",
+        when: (answers) => answers.title === 'Manager'
+    },
+    {
+        type: "confirm",
+        name: "anotherEmployee",
+        message: "Would you like to add another employee?"    
+    }
+]
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+//uses inquirer to prompt user for info
+const addEmployee = () => {
+    inquirer
+    .prompt(prompts)
+    .then((answers) => {
+        switch (answers.title) {
+            case 'Manager':
+                employees.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber));
+            break;
+            case 'Engineer':
+                employees.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
+            break;
+            case 'Intern':
+                employees.push(new Intern(answers.name, answers.id, answers.email, answers.school));
+            break;
+        }
+        if (answers.anotherEmployee) {
+            addEmployee();
+        } else {
+            fs.writeFile(outputPath, render(employees), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+        }
+    });
+}
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+function init() {
+    addEmployee();
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+init();
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,32 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+//////        id = lastId++;
+        this.email = email;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    getRole() {
+        return `Employee`;
+    }
+}
+
+/////Employee.lastId = 0;
+
+module.exports = Employee;
+
+const e = new Employee("Nardin", 1, "nardin@TextDecoderStream.com");
+console.log(e);
